d756b16a94362fba7b4cc238c55c1df5e80023c0
karar3k <karar3k@yahoo.com>
2022-10-20 17:56:00 +0300

    First

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const patientsRouter = require('./src/routes/v1/patients')
+const historyRouter = require('./src/routes/v1/history')
 
 const PORT = 5000;
 // middlwares
@@ -14,5 +15,6 @@ app.get('/', (req, res)=>{
 // call routers
 
 app.use('/api/v1/patients',patientsRouter);
+app.use('/api/v1/history',historyRouter);
 
 app.listen(PORT)
