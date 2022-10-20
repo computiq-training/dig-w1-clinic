96b30f56b7066e3d5c02aa3efea1b20a61af773f
ZAIA-AK <zainabakd94@gmail.com>
2022-10-20 03:35:01 +0300

    zainab try

--- src/controllers/v1/patients/PatientController.js
+++ src/controllers/v1/patients/PatientController.js
@@ -9,7 +9,7 @@ const patients = [
         phone:"+9647711332225"
     },
     {
-        id:1,
+        id:2,
         full_name:"Sara Ali",
         birth_date:"01/10/2000",
         gender:"female",
@@ -32,22 +32,83 @@ const getAllPatients = (req, res)=>{
 const getPatientById = (req, res)=>{
     const id = req.params.id;
     // TO-DO
+    let PatientById = PatientById.find((item)=> item.id == id)
+    if(!PatientById)
+        response.status(404).json('No record for this patient')
+    response.status(200).json(PatientById)
+    
 }
 
 const deletePatient = (req, res)=>{
     const id = req.params.id;
     // TO-DO
+    let deletePatientIndex = Patients.findindex((item)=> item.id == id)
+    if(deletePatientIndex > -1)
+        patients.splice(deletePatientIndex,1)
+        response.status(404).json('No record for this patient')
+        response.status(200).json(success(200, patients,"Current patient entry deleted"))
+    // if(deletePatientByIdIndex > -1)
+    // {
+    //     patients.splice(deletePatientByIdIndex,1)
+    //     response.status(200).json(success(200, patients,"Current patient entry deleted"))
+    // }
+    // else{
+    //     response.status(404).json('No record for this patient')
+    // }
+        
+    
 }
 
 const updatePatient = (req, res)=>{
     const id = req.params.id;
     // TO-DO
+    const {email} = req.body
+    let updatePatientIndex = Patients.findIndex((item)=> item.id == id)
+    if(updatePatientIndex > -1)
+    {
+        let newPatient = {
+            id:id,
+            full_name:full_name,
+            birth_date:birth_date,
+            gender:gender,
+            code:code,
+            phone:phone,
+            email: email
+        }
+    }
+        patients[updatePatientIndex] = newPatient; 
+
+        response.status(404).json('No record for this patient')
+        response.status(200).json(success(200, patients,"Current patient entry deleted"))
 }
 
 const getHistoryOfPatient = (req, res)=>{
     const id = req.params.id; // patient id
     // TO-DO
+    let HistoryOfPatient = history.find((item)=> item.id == id)
+    if(!HistoryOfPatient)
+        response.status(404).json('No history for this patient')
+    response.status(200).json(HistoryOfPatient)
+
+
     return res.status(200).json(success(200,{},"Ok"))
+}
+
+//create new patient
+const addNewPatient = (req, res)=>{
+    let {
+        id, full_name, birth_date,gender, code, phone
+    } = request.body
+    patients.push({
+        id:id,
+        full_name:full_name,
+        birth_date:birth_date,
+        gender:gender,
+        code:code,
+        phone:phone
+    })
+    return res.status(200).json(success(200,patients,"New patient case created"))
+
 }
 
 module.exports = {
@@ -55,5 +116,6 @@ module.exports = {
     getPatientById,
     deletePatient,
     updatePatient,
-    getHistoryOfPatient
+    getHistoryOfPatient,
+    addNewPatient
 }
--- src/routes/v1/patients.js
+++ src/routes/v1/patients.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getAllPatients, getPatientById, deletePatient, updatePatient, getHistoryOfPatient} = require('../../controllers/v1/patients/PatientController')
+const {getAllPatients, getPatientById, deletePatient, updatePatient, getHistoryOfPatient, addNewPatient} = require('../../controllers/v1/patients/PatientController')
 
 
 
@@ -10,4 +10,6 @@ router.delete('/:id', deletePatient);
 router.put('/:id', updatePatient);
 router.get('/:id/history', getHistoryOfPatient);
 // TO-DO // add endpoint for adding new Patient
+router.post('/', addNewPatient);
+
 module.exports = router;
