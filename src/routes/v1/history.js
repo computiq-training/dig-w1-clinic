852e320a843f2087526d809d02de9255a65f3716
movy18x <movy18x@gmail.com>
2022-10-20 09:17:18 +0300

    solved

--- src/routes/v1/history.js
+++ src/routes/v1/history.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const {getAllHistory} = require('../../controllers/v1/history/HistoryController')
+const express = require("express");
+const router = express.Router();
+const {
+  getAllHistory,
+  createHistoryForP,
+} = require("../../controllers/v1/history/HistoryController");
 
-
-router.get('/', getAllHistory);
-// TO-DO // add endpoint for posting new history for a patient
+router.get("/", getAllHistory);
+router.post("/:id", createHistoryForP);
 module.exports = router;
